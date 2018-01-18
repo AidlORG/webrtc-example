f8ab4461c6f2a2beb755d1205fcc3dd4ec735a78
David Jimenez <dajimenezja@gmail.com>
2018-01-18 01:47:01 -0700

    Implement webcam initializer

--- app/src/main/java/com/example/webrtc/MainActivity.java
+++ app/src/main/java/com/example/webrtc/MainActivity.java
@@ -1,13 +1,138 @@
 package com.example.webrtc;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.CameraEnumerator;
+import org.webrtc.EglBase;
+import org.webrtc.MediaConstraints;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CAMERA = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPermissions();
+
+    }
+
+    private void initializeWebRTC() {
+        //Initialize PeerConnectionFactory globals.
+        //Params are context, initAudio,initVideo and videoCodecHwAcceleration
+        PeerConnectionFactory.initialize(
+                PeerConnectionFactory.InitializationOptions.builder(this)
+                        .setEnableVideoHwAcceleration(true)
+                        .createInitializationOptions()
+        );
+
+        //Create a new PeerConnectionFactory instance.
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        PeerConnectionFactory peerConnectionFactory = new PeerConnectionFactory(options);
+
+
+        //Now create a VideoCapturer instance. Callback methods are there if you want to do something! Duh!
+        VideoCapturer videoCapturerAndroid = createVideoCapturer();
+        //Create MediaConstraints - Will be useful for specifying video and audio constraints. More on this later!
+        MediaConstraints constraints = new MediaConstraints();
+
+        //Create a VideoSource instance
+        VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturerAndroid);
+        VideoTrack localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
+
+        //create an AudioSource instance
+        AudioSource audioSource = peerConnectionFactory.createAudioSource(constraints);
+        AudioTrack localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+
+        //we will start capturing the video from the camera
+        //params are width,height and fps
+        videoCapturerAndroid.startCapture(1000, 1000, 30);
+
+        //create surface renderer, init it and add the renderer to the track
+        SurfaceViewRenderer videoView = (SurfaceViewRenderer) findViewById(R.id.surface_rendeer);
+        videoView.setMirror(true);
+
+        EglBase rootEglBase = EglBase.create();
+        videoView.init(rootEglBase.getEglBaseContext(), null);
+
+        localVideoTrack.addRenderer(new VideoRenderer(videoView));
+    }
+
+    private void getPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+                //TODO
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, PERMISSION_CAMERA);
+            }
+        } else {
+            initializeWebRTC();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initializeWebRTC();
+                } else {
+                    //TODO
+                }
+                return;
+            }
+        }
+    }
+
+    private VideoCapturer createVideoCapturer() {
+        VideoCapturer videoCapturer;
+        videoCapturer = createCameraCapturer(new Camera1Enumerator(false));
+        return videoCapturer;
+    }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        // Trying to find a front facing camera!
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        // We were not able to find a front cam. Look for other cameras
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
     }
 }
